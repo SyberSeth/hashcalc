6eabe5ec86513c2666a46101c266381da9a5e0a2
SyberSeth <44422337+SyberSeth@users.noreply.github.com>
2018-10-24 11:31:24 -0600

    Add files via upload

--- MD5compare.py
+++ MD5compare.py
@@ -3,6 +3,7 @@ import tkFileDialog
 import hashlib
 import os
 import datetime
+import difflib
 
 def main():
 	#call Tkinter for user input on file selection via GUI.
@@ -11,26 +12,15 @@ def main():
 	print "Please select two md5 files to compare"
 	file1 = tkFileDialog.askopenfilename()
 	file2 = tkFileDialog.askopenfilename()
-	f1_line = file1.readline()
-	f2_line = file2.readline()
-		#keep track of lines read in file
-	line_counter=1
-		#visually show which files are being compared
-	print("Now comparing files", file1, "and", file2)
-		# Loop if either file1 or file2 has not reached EOF
-'''
-	while f1_line != '' or f2_line != '':
-		if f1_line != f2_line:
-			if f2_line == '' and f1_line != '':
-				print(">+", "Line-%d" % line_no, f1_line)
-		else f1_line != '':
-				print(">", "Line-%d" % line_no, f1_line)
-					
-		f1_line = file1.readline()
-		f2_line = file2.readline()
-			
-		line_counter += 1
-'''
+	with open(file1) as f1:
+			f1_data = f1.read()
+	with open(file2) as f2:
+			f2_data = f2.read()
+	print "Any differences in MD5s between files will be displayed below. A blank output means no changes"
+# Find and print the diff:
+	for line in difflib.unified_diff(f1_data, f2_data, fromfile = 'file1', tofile='file2', lineterm=''):
+		print line
+		
 		
 if __name__ == "__main__":
     main()
